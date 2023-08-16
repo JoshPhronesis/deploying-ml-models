16dd93e424dc0bf0c5b25f7cb639e7237622a8bc
joshua.akosa <akosajoshua@yahoo.com>
2023-08-16 16:35:03 +0200

    implementation

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the saved model
+with open('kings_county_model.pkl', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    new_data = np.array(data['data']).reshape(-1, 1)
+    prediction = loaded_model.predict(new_data)
+    return jsonify(prediction.tolist())
+
+if __name__ == '__main__':
+    app.run(debug=True)
